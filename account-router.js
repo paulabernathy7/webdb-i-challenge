9911ae3f116d1da07c67072c6ea3594627c9c958
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-07 23:42:17 -0400

    get requests work

--- account-router.js
+++ account-router.js
@@ -9,6 +9,7 @@ const router = express.Router();
 //   res.send("<h3>Works!</h3>");
 // });
 
+//read
 router.get("/", async (req, res) => {
   try {
     const accounts = await db("accounts");
@@ -16,6 +17,20 @@ router.get("/", async (req, res) => {
     res.status(200).json(accounts);
   } catch (err) {
     res.status(500).json({ message: "error getting data", error: err.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [accounts] = await db("accounts").where({ id }); // destructing an array. Only use if there is an array full of objects
+    if (accounts) {
+      res.status(200).json(accounts);
+    } else {
+      res.status(404).json({ message: "id could not be found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error getting data" });
   }
 });
 
